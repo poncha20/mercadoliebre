a276754c7266d7d156a13d46251955a32d39a037
poncha20 <113553844+poncha20@users.noreply.github.com>
2022-10-20 19:59:03 -0300

    module 2

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app=express();
 const path=require("path");
 const publicPath=path.resolve(__dirname,"./public");
 app.use(express.static(publicPath))
-const port=process.env.port||3001;
-app.listen(port,()=>{
+const port=process.env.PORT||3001;
+app.listen(PORT,()=>{
     console.log("servidor levantado");
 });
 
